8a5ac00e50e3bb7343ac92057f8927de2fa107c8
Hussain_Sharif <122522406+Hussain-Sharif@users.noreply.github.com>
2024-01-27 23:31:44 +0530

    Update styledComponents.js {SideBar}

--- src/components/SideBar/styledComponents.js
+++ src/components/SideBar/styledComponents.js
@@ -1,9 +1,5 @@
-import {Link} from 'react-router-dom'
 
-import {AiFillHome} from 'react-icons/ai'
-import {BsFire} from 'react-icons/bs'
-import {PiGameControllerFill} from 'react-icons/pi'
-import {MdOutlinePlaylistAdd} from 'react-icons/md'
+
 import styled from 'styled-components'
 
 export const SideBarBg = styled.div`
@@ -32,57 +28,7 @@ export const SideBarLinks = styled.ul`
   //   border: solid red 1px;
 `
 
-export const NavigatingLink = styled(Link)`
-  width: 100%;
-  text-decoration: none;
-  cursor: pointer;
-  padding-top: 3px;
-  padding-bottom: 3px;
-  //   border: solid orange 1px;
-`
 
-export const HomeIcon = styled(AiFillHome)`
-  font-size: 25px;
-  color: ${props => (!props.isItemSelected ? '#606060' : 'red')};
-`
-export const TrendingIcon = styled(BsFire)`
-  font-size: 25px;
-  color: ${props => (!props.isItemSelected ? '#606060' : 'red')};
-`
-export const GamingIcon = styled(PiGameControllerFill)`
-  font-size: 25px;
-  color: ${props => (!props.isItemSelected ? '#606060' : 'red')};
-`
-export const SavedVideosIcon = styled(MdOutlinePlaylistAdd)`
-  font-size: 25px;
-  color: ${props => (!props.isItemSelected ? '#606060' : 'red')};
-`
-
-export const SideBarLink = styled.li`
-  //   border: solid blue 1px;
-  width: 100%;
-  padding-left: 10px;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-  background-color: ${props => {
-    const {isBrighterTheme, isItemSelected} = props
-    console.log('------------->>>', {isBrighterTheme, isItemSelected})
-    if (isBrighterTheme && isItemSelected) {
-      return '#F1F5F9'
-    }
-    if (!isBrighterTheme && isItemSelected) {
-      return '#383838'
-    }
-    return 'auto'
-  }};
-
-  :hover {
-    background-color: ${props =>
-      props.isBrighterTheme ? '#F1F5F9' : '#383838'};
-  }
-`
 export const SideBarNavItem = styled.p`
   margin: 0;
   margin-left: 15px;
